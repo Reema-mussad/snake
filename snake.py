5a644fa0d59035b9e194677986d379486da600d3
reema <reema.alshatiri@gmail.com>
2025-11-04 20:39:40 +0300

    first upload

---
+++ snake.py
@@ -0,0 +1,69 @@
+import random
+import curses
+
+screen = curses.initscr()
+#for hiding mouse
+curses.curs_set(0)
+
+#set the window hight and width
+height, width= screen.getmaxyx()
+
+#creat a window from curses 
+window = curses.newwin(height, width,0 ,0)
+
+#read the botton
+window.keypad(1)
+
+#update
+window.timeout(100)
+
+# set coordinates for head
+head_x = width  // 2
+head_y = height // 4
+
+snake = [[head_y,head_x],[head_y,head_x-1],[head_y,head_x-2]]
+#where the food will show
+food = [height // 2, width // 2]
+window.addch(food[0],food[1],curses.ACS_PI)
+
+# where to go by defult
+key = curses.KEY_RIGHT
+
+
+#snake movement
+while True: 
+    key_next = window.getch()
+    #-1 in .getch() means nothing pressed
+    
+    key = key if key_next == -1 else key_next
+#snake head hit the height Y or width X or head hit the body
+    if snake[0][0] in [0,height] or snake[0][1] in [0,width] or snake[0] in snake[1:]:
+        curses.endwin()
+        quit()
+#snake movement
+    new_head = [snake[0][0],snake[0][1]]
+
+    if key == curses.KEY_DOWN :
+        new_head[0] += 1
+    if key == curses.KEY_UP :
+        new_head[0] -= 1
+    if key == curses.KEY_RIGHT :
+        new_head[1] += 1
+    if key == curses.KEY_LEFT :
+        new_head[1] -= 1
+
+    snake.insert(0,new_head)
+#show food in random places
+    if snake[0] == food :
+        food = None
+        while food == None:
+            new_food = [random.randint(1,height-1),random.randint(1,width-1)]
+            food = new_food if new_food not in snake else None
+        window.addch(food[0],food[1],curses.ACS_PI)
+    else:
+        tail = snake.pop()
+        window.addch(tail[0],tail[1], ' ')
+
+    window.addch(snake[0][0],snake[0][1],curses.ACS_CKBOARD)
+
+    window.refresh()
---
+++ test.py
@@ -0,0 +1,30 @@
+from math import *
+#girl_name = input("Enter a girl name: ")
+#boy_name = input("Enter a boy name: ")
+#place_name = input("Enter a name for any place: ")
+#char_name = input("a characteristic: ")
+#what_name = input("what he or she doing: ")
+#something_name = input("something: ")
+#anemal_name = input("Enter an anemal: ")
+#print("one day "+girl_name+" and "+boy_name)
+#print("decided to go to the "+place_name+" together everything seemed normal until things got "+char_name)
+#print("suddenly "+girl_name+" Started "+what_name +" while holding a "+something_name)
+#print("Out of nowhere a "+anemal_name+" appeareded and calmly sat next to them like it came to order lunch too")
+frends = ["amal"," joud","rahaf","dana"]
+print(frends[1:3])
+num = 10.5
+numb = pow(2,4)
+print(sqrt(num))
+print(numb)
+nn = abs(100.33)
+print(round(num))
+def maxn (num1 ,num2 ,num3):
+    if num1 > num2 and num1 > num3:
+        return num1
+    elif num2 > num1 and num2 > num3:
+        return num2
+    else:
+        return num3
+
+print(maxn(45,66,34))
+print(max(666,576,456))
